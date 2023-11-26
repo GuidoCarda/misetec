09c6b0c21b635db3b993f4b859716e448ab707a8
Guido Cardarelli <guidocarda@hotmail.com>
2023-11-26 02:11:49 -0300

    Added client and order models

---
+++ server/src/api/clients/clients.model.ts
@@ -0,0 +1,18 @@
+import { z } from "zod";
+
+export const ClientSchema = z.object({
+  id: z.number().int().positive(),
+  firstname: z.string().max(50),
+  lastname: z.string().max(50),
+  address: z.string().max(100),
+  phone_number: z.string().max(20),
+  postal_code: z.string().max(10),
+  email: z.string().email(),
+});
+
+export const CreateClientSchema = ClientSchema.omit({ id: true });
+export const UpdateClientSchema = ClientSchema.omit({ id: true });
+
+export type Client = z.infer<typeof ClientSchema>;
+export type CreateClient = z.infer<typeof CreateClientSchema>;
+export type UpdateClient = z.infer<typeof UpdateClientSchema>;
---
+++ server/src/api/orders/orders.model.ts
@@ -0,0 +1,47 @@
+import { z } from "zod";
+
+export const OrderSchema = z.object({
+  id: z.number().positive(),
+  created_at: z.date(),
+  finished_at: z.date().optional(),
+  description: z.string(),
+  device_failure: z.string().optional(),
+  accesories: z.string().optional(),
+  report: z.string().optional(),
+  service_type_id: z.number().positive(),
+  status_id: z.number().positive().default(1),
+  device_id: z.number().positive().optional(),
+  client_id: z.number().positive(),
+  staff_id: z.number().positive(),
+});
+
+export const CreateOrderSchema = OrderSchema.omit({
+  id: true,
+  created_at: true,
+  status_id: true,
+  finished_at: true,
+});
+
+export const UpdateOrderSchema = OrderSchema.omit({
+  id: true,
+  created_at: true,
+  client_id: true,
+  staff_id: true,
+});
+
+export const UpdateOrderStateSchema = z.object({
+  status_id: z.number().positive(),
+  finished_at: z.date().optional(),
+});
+
+export const OrderQueryParamsSchema = z.object({
+  status_id: z.string().optional(),
+  service_type_id: z.string().optional(),
+  client_id: z.string().optional(),
+});
+
+export type Order = z.infer<typeof OrderSchema>;
+export type CreateOrder = z.infer<typeof CreateOrderSchema>;
+export type UpdateOrder = z.infer<typeof UpdateOrderSchema>;
+export type UpdateOrderState = z.infer<typeof UpdateOrderStateSchema>;
+export type OrderQueryParams = z.infer<typeof OrderQueryParamsSchema>;
