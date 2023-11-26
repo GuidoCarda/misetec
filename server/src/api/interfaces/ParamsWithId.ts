a7ddf664df372b6fc307ee813060c3f8aae8d418
Guido Cardarelli <guidocarda@hotmail.com>
2023-11-26 02:11:15 -0300

    Added request validation middleware and request validators

--- server/src/api/interfaces/ParamsWithId.ts
+++ server/src/api/interfaces/ParamsWithId.ts
@@ -1,3 +1,13 @@
-export default interface ParamsWithId {
-  id: string;
-}
+import { z } from "zod";
+
+export const ParamsWithIdSchema = z.object({
+  id: z.string().refine(
+    (v) => {
+      let n = Number(v);
+      return !isNaN(n) && v?.length > 0;
+    },
+    { message: "Invalid number" }
+  ),
+});
+
+export type ParamsWithId = z.infer<typeof ParamsWithIdSchema>;
--- server/src/api/middlewares.ts
+++ server/src/api/middlewares.ts
@@ -1,5 +1,33 @@
 import { Request, Response, NextFunction } from "express";
+
 import ErrorResponse from "./interfaces/ErrorResponse";
+import RequestValidators from "./interfaces/RequestValidators";
+import { ZodError } from "zod";
+
+export function validateRequest(validators: RequestValidators) {
+  return async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      if (validators.params) {
+        req.params = await validators.params.parseAsync(req.params);
+      }
+
+      if (validators.body) {
+        req.body = await validators.body.parseAsync(req.body);
+      }
+
+      if (validators.query) {
+        req.query = await validators.query.parseAsync(req.query);
+      }
+      next();
+    } catch (error) {
+      console.log(error);
+      if (error instanceof ZodError) {
+        res.status(422);
+      }
+      next(error);
+    }
+  };
+}
 
 export function errorHandler(
   err: Error,
