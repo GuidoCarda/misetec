2482324a0eb1ec70280e5c491d12555530079509
Guido Cardarelli <guidocarda@hotmail.com>
2024-01-16 23:43:36 -0300

    refactored auth api services

--- client/src/services/auth.ts
+++ client/src/services/auth.ts
@@ -1,82 +1,31 @@
 import { clientLoginFormSchema } from "@/pages/login/ClientsLoginForm";
 import { loginFormSchema } from "@/pages/login/StaffLoginForm";
+import api from "@/services/config";
 import { z } from "zod";
 
 type StaffLoginParams = z.infer<typeof loginFormSchema>;
+type ClientLoginParams = z.infer<typeof clientLoginFormSchema>;
 
 export async function staffLogin(values: StaffLoginParams) {
-  const res = await fetch("http://localhost:3000/api/v1/auth/login", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(values),
-  });
-
-  const data = await res.json();
-
-  if (res.status === 400) {
-    throw new Error(data.message);
-  }
-
-  return data;
+  const response = await api.post("/auth/login", values);
+  return response.data;
 }
 
 export async function logout() {
   window.localStorage.removeItem("user");
 }
 
-type ClientLoginParams = z.infer<typeof clientLoginFormSchema>;
-
 export async function clientLogin(values: ClientLoginParams) {
-  const res = await fetch("http://localhost:3000/api/v1/auth/client-login", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(values),
-  });
-
-  const data = await res.json();
-
-  if (res.status === 400) {
-    throw new Error(data.message);
-  }
-
-  return data;
+  const response = await api.post("/auth/client-login", values);
+  return response.data;
 }
 
 export async function clientLoginConfirmation(values: { otp: string }) {
-  const res = await fetch("http://localhost:3000/api/v1/auth/client-otp", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(values),
-  });
-
-  const data = await res.json();
-
-  if (res.status === 400) {
-    throw new Error(data.message);
-  }
-
-  return data;
+  const response = await api.post("/auth/client-otp", values);
+  return response.data;
 }
 
 export async function deleteStaffMember(id: string) {
-  const res = await fetch(`http://localhost:3000/api/v1/auth/staff/${id}`, {
-    method: "DELETE",
-    headers: {
-      "Content-Type": "application/json",
-    },
-  });
-
-  const data = await res.json();
-
-  if (res.status === 400) {
-    throw new Error(data.message);
-  }
-
-  return data;
+  const response = await api.delete(`/auth/staff/${id}`);
+  return response.data;
 }
