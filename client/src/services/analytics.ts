baeade416683efede323ff3cfb5ccc6fce9c2c70
Guido Cardarelli <guidocarda@hotmail.com>
2024-01-19 13:16:20 -0300

    refactored analytics api service

--- client/src/services/analytics.ts
+++ client/src/services/analytics.ts
@@ -1,23 +1,19 @@
+import api from "@/services/config";
 import { format } from "date-fns";
 import { DateRange } from "react-day-picker";
 
 export async function getAnalytics(date: DateRange | undefined) {
-  const url = new URL("http://localhost:3000/api/v1/analytics");
+  const params = new URLSearchParams();
 
   if (date) {
     const { from: fromDate, to: toDate } = date as DateRange;
-
-    const params = new URLSearchParams({
-      start: format(fromDate!, "yyyy-MM-dd"),
-    });
+    params.append("start", format(fromDate!, "yyyy-MM-dd"));
 
     if (toDate) {
       params.append("end", format(toDate!, "yyyy-MM-dd"));
     }
-    url.search = params.toString();
   }
 
-  const res = await fetch(url);
-  const data = await res.json();
-  return data;
+  const response = await api.get("/analytics?" + params.toString());
+  return response.data;
 }
